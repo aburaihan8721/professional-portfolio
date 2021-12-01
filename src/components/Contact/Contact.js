d1f8388de576cdb52b18d63ee94edc1234306c57
Abu Raihan <aburaihan8721@gmail.comm>
2021-12-01 13:07:59 +0600

    email process added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+
 import "./Contact.css";
 import contactImg from "../../images/about.jpg";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_gmredqd", "template_4n2tlci", form.current, "user_dok71YYOkH45NqWB4rswb").then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+    setSuccess(true);
+    e.target.reset();
+  };
+
   return (
     <div className="contact component__space" id="contact">
       <div className="container">
@@ -16,15 +36,20 @@ const Contact = () => {
                   <strong>+088 01718562225</strong> or email <strong>aburaihan8721@gmail.com</strong>
                 </p>
               </div>
-              <div className="input__box">
-                <input type="text" className="contact name" placeholder="Your name *" />
-                <input type="text" className="contact email" placeholder="Your Email *" />
-                <input type="text" className="contact subject" placeholder="Write a Subject" />
-                <textarea name="message" id="message" placeholder="Write Your message"></textarea>
+              <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name="name" className="contact name" placeholder="Your name *" />
+                <input type="email" name="email" className="contact email" placeholder="Your Email *" />
+                <input type="text" name="subject" className="contact subject" placeholder="Write a Subject" />
+                <textarea name="message" id="message" placeholder="Write Your message" />
                 <button className="btn contact pointer" type="submit">
                   Submit
                 </button>
-              </div>
+                {success && (
+                  <div className="alert alert-primary" role="alert">
+                    Your message has been submitted successfully!
+                  </div>
+                )}
+              </form>
             </div>
           </div>
           <div className="col-md-6">
